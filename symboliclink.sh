95dfad2b1186c665ab3dc2d20dce3a0835f1f2cd
muratakashi <muratakashi@gmail.com>
2019-12-19 21:56:21 +0900

    zprofileを追加しました

--- symboliclink.sh
+++ symboliclink.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/git/dotfiles/.bash_profile ~/.bash_profile
+# ln -sf ~/git/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/git/dotfiles/.zprofile ~/.zprofile
